50ae2973e3fd9ae6c302973baeab8ba8db2b615c
takobell1988 <59435144+takobell1988@users.noreply.github.com>
2020-01-29 16:43:08 +0200

    score.py work
    ork with difficulty and writh to score.txt file

--- Score.py
+++ Score.py
@@ -1,11 +1,36 @@
 from Live import load_game
 
-
-def points_of_winning():
-    gamevalue = load_game()
-    score = (gamevalue[1] * 3) + 5
-    print(score)
-    return score
+gamevalues = load_game()
+difficulty = gamevalues[1]
 
 
-points_of_winning()
+# def points_of_winning():
+#     gamevalue = load_game()
+#     difficulty = (gamevalue[1] * 3) + 5
+#     print(difficulty)
+#     return difficulty
+
+#
+# points_of_winning()
+
+def add_score(difficulty):
+    try:
+        with open('Scores.txt', 'r') as scores:
+            current_score = scores.read()
+            print("score.txt points: " + str(current_score))
+    except BaseException as e:
+        print("cannot read scores.txt file, creating new one")
+        open("Scores.txt", "w+")
+
+    points_of_winning = (difficulty * 3) + 5
+    print("win points: " + str(points_of_winning))
+    new_score = int(current_score) + points_of_winning
+    print("new score is : " + str(new_score))
+    a = open('Scores.txt', 'w+')
+    a.write(str(new_score))
+
+
+
+
+
+add_score(difficulty)
