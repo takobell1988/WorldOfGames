5730c5098f0d444d04db99ce2b57f4e06f2ff46a
takobell1988 <59435144+takobell1988@users.noreply.github.com>
2020-02-02 16:01:31 +0200

    create score file at start
    score file created when you run MainGame.py

--- MainGame.py
+++ MainGame.py
@@ -1,7 +1,8 @@
 import time
 import os
 from Live import load_game, welcome
-from CurrencyRouletteGame import *
+from Score import create_score_file
+create_score_file()
 input_name = input('Enter yor name here: ')
 start_over = 0
 
--- Score.py
+++ Score.py
@@ -1,24 +1,25 @@
+def create_score_file():
+    file = open('Scores.txt', 'w')
+    file.write("0")
+    file.close()
+
 
 def add_score(difficulty):
     try:
-        with open('Scores.txt', 'r') as scores:
-            current_score = scores.read()
-            if current_score == None:
-                a = open('Scores.txt', 'w+')
-                a.write("0")
-            print("old score points: " + str(current_score))
-    except BaseException as e:
-        print("cannot read scores.txt file, creating new one")
-        # a = open('Scores.txt', 'w+')
-        # a.write("0")
-        current_score = 0
+        file = open('Scores.txt', 'r')
+        current_score = file.read()
+        print("old score points: " + str(current_score))
+        points_of_winning = (difficulty * 3) + 5
+        print("win points: " + str(points_of_winning))
+        new_score = int(current_score) + int(points_of_winning)
+        print("new score is : " + str(new_score))
+        file = open('Scores.txt', 'w')
+        file.write(str(new_score))
+        file.close()
 
-    points_of_winning = (difficulty * 3) + 5
-    print("win points: " + str(points_of_winning))
-    new_score = int(current_score) + int(points_of_winning)
-    print("new score is : " + str(new_score))
-    a = open('Scores.txt', 'w')
-    a.write(str(new_score))
+    except BaseException as e:
+        print("ERROR : cannot read scores.txt file")
+
 
 
 
