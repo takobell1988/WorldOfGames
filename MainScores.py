306ba2190c86411b7d48497b208fc1d624f67db3
takobell1988 <59435144+takobell1988@users.noreply.github.com>
2020-02-02 16:45:28 +0200

    added flask HTML
    work with flask html code

--- MainScores.py
+++ MainScores.py
@@ -2,9 +2,24 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def score_server():
+    file = open('Scores.txt', 'r')
+    score = file.read()
+    print(score)
+    return score
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def run_page():
+    return '<html>' \
+               '<head>' \
+               '<title>Scores Game</title>' \
+               '</head>' \
+               '<body>' \
+                    '<h1>The score is <div id="score">' + score_server() + '</div></h1>' \
+               '</body>' \
+           '</html>'
+
 
 
 if __name__ == '__main__':
