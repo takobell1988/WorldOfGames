e674ebdd333c1ee38f356a091fed16e3a331ce5c
takobell1988 <59435144+takobell1988@users.noreply.github.com>
2020-02-16 16:47:54 +0200

    selenuim configuratuions1

--- e2e.py
+++ e2e.py
@@ -8,21 +8,24 @@ def test_scores_service():
     try:
         Chrome_driver = webdriver.Chrome(executable_path='C:\Temp\chromedriver.exe')
         Chrome_driver.get('http://localhost:5000/')
-        Chrome_driver.find_element_by_id("videoURL").send_keys(str(line))
-        Chrome_driver.find_element_by_xpath(
-            '/html/body/div[2]/div[1]/div/div/div/form/select/optgroup/option[3]').click()
-        sleep(2)
-        Chrome_driver.find_element_by_name("submitForm").click()
-        sleep(80)
-        Chrome_driver.find_element_by_xpath('//*[@id="conversionSuccess"]/p[4]/a').click()
-        sleep(80)
-        print("success downloading : " + str(line))
-        Chrome_driver.close()
+        score_value = Chrome_driver.find_element_by_id("score").text
+        print(score_value)
+        # Chrome_driver.find_element_by_xpath(
+        #     '/html/body/div[2]/div[1]/div/div/div/form/select/optgroup/option[3]').click()
+        # sleep(2)
+        # Chrome_driver.find_element_by_name("submitForm").click()
+        # sleep(80)
+        # Chrome_driver.find_element_by_xpath('//*[@id="conversionSuccess"]/p[4]/a').click()
+        # sleep(80)
+        # print("success downloading : " + str(line))
+        # Chrome_driver.close()
     except BaseException as e:
         print(e.args)
-        print("could not download : " + str(line))
-        Chrome_driver.close()
-    else:
-        # contents.close()
-        print("Finish !")
+        print("could not download : ")
+        # Chrome_driver.close()
+    # else:
+    #     # contents.close()
+    #     print("Finish !")
+
+test_scores_service()
 
