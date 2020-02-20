bc66d5e9c33f0530e71c0087771ccddfac02021c
takobell1988 <59435144+takobell1988@users.noreply.github.com>
2020-02-20 15:14:54 +0200

    setting main function

--- e2e.py
+++ e2e.py
@@ -4,25 +4,31 @@ from time import sleep
 
 
 def test_scores_service():
-    print("selenium do tests")
-    try:
-        Chrome_driver = webdriver.Chrome(executable_path='C:\Temp\chromedriver.exe')
-        Chrome_driver.get('http://localhost:5000/')
-        score_value = Chrome_driver.find_element_by_id("score").text
+    Chrome_driver = webdriver.Chrome(executable_path='C:\Temp\chromedriver.exe')
+    Chrome_driver.get('http://localhost:5000/')
+    score_value = Chrome_driver.find_element_by_id("score").text
 
-        if 1 < int(score_value) < 1000:
-            print(score_value)
-            return True
-        else:
-            print('test not pass!')
-            return False
+    if 1 < int(score_value) < 1000:
+        return True
         Chrome_driver.close()
+    else:
+        print('test not pass!')
+        return False
+        Chrome_driver.close()
+
+
+def main_function():
+    try:
+        a = test_scores_service()
+        if a is True:
+            print('finished sucessfully 0 ')
+            return 0
+        else:
+            print('error , finished with exit code -1 ')
+            return -1
     except BaseException as e:
         print(e.args)
         print("ERROR : could not perform test")
-        Chrome_driver.close()
 
 
-
-test_scores_service()
-
+main_function()
